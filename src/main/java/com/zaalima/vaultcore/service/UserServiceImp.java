28a68700365fcb1541c2b7e7bce18082b1905ad4
Nitin <rajnitin793@gmail.com>
2026-04-10 20:30:13 +0530

    implement user and role management with transactional integrity

--- src/main/java/com/zaalima/vaultcore/service/UserServiceImp.java
+++ src/main/java/com/zaalima/vaultcore/service/UserServiceImp.java
@@ -5,38 +5,42 @@ import com.zaalima.vaultcore.domain.User;
 import com.zaalima.vaultcore.repo.RoleRepo;
 import com.zaalima.vaultcore.repo.UserRepo;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service @RequiredArgsConstructor
+@Service @RequiredArgsConstructor @Transactional @Slf4j
 
 public class UserServiceImp implements UserService {
     private final UserRepo userRepo;
     private final RoleRepo roleRepo;
     @Override
     public User saveUser(User user) {
-        return null;
+        return userRepo.save(user);
     }
 
     @Override
     public Role saveRole(Role role) {
-        return null;
+        return roleRepo.save(role);
     }
 
     @Override
     public void addRoleToUser(String username, String roleName) {
-
+        User user = userRepo.findbyUsername(username);
+        Role role = roleRepo.findbyName(roleName);
+        user.getRoles().add(role);
     }
 
     @Override
     public User getUser(String username) {
-        return null;
+        return userRepo.findbyUsername((username));
     }
 
     @Override
     public List<User> getUser() {
-        return List.of();
+        return userRepo.findAll();
     }
 }
