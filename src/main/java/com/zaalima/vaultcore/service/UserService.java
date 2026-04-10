e0c3d95c44a2a7322fd3ffe37366141de003f8c4
Nitin <rajnitin793@gmail.com>
2026-04-10 20:03:29 +0530

    added UserService

---
+++ src/main/java/com/zaalima/vaultcore/service/UserService.java
@@ -0,0 +1,14 @@
+package com.zaalima.vaultcore.service;
+
+import com.zaalima.vaultcore.domain.Role;
+import com.zaalima.vaultcore.domain.User;
+
+import java.util.List;
+
+public interface UserService {
+    User saveUser(User user);
+    Role saveRole(Role role);
+    void addRoleToUser(String username, String roleName);
+    User getUser(String username);
+    List<User> getUser();
+}
