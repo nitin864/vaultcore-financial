040779d27eb0aa051b8957ed783ccdd20b5ead69
Nitin <rajnitin793@gmail.com>
2026-04-10 20:50:28 +0530

    updates

---
+++ src/main/java/com/zaalima/vaultcore/api/UserResource.java
@@ -0,0 +1,24 @@
+package com.zaalima.vaultcore.api;
+
+import com.zaalima.vaultcore.domain.User;
+import com.zaalima.vaultcore.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserResource {
+    private final UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>>getUsers(){
+        return ResponseEntity.ok().body(userService.getUser());
+    }
+}
