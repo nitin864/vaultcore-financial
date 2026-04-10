ef7b1ff8e16dc4b2ff8cdcb62e5a41b5e07d4575
Nitin <rajnitin793@gmail.com>
2026-04-10 12:58:53 +0530

    added missing constructors

--- src/main/java/com/zaalima/vaultcore/domain/Role.java
+++ src/main/java/com/zaalima/vaultcore/domain/Role.java
@@ -1,4 +1,14 @@
 package com.zaalima.vaultcore.domain;
 
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Role {
 }
--- src/main/java/com/zaalima/vaultcore/domain/User.java
+++ src/main/java/com/zaalima/vaultcore/domain/User.java
@@ -1,13 +1,15 @@
 package com.zaalima.vaultcore.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ManyToAny;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
-@Entity @Data
+@Entity @Data @NoArgsConstructor @AllArgsConstructor
 public class User {
     @Id @GeneratedValue(strategy =  GenerationType.AUTO)
     private Long id;
