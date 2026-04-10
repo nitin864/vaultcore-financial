5966ef88e3e307976cc17b5ce134514d1e7dc0a0
Nitin <rajnitin793@gmail.com>
2026-04-10 19:44:48 +0530

    added user and role Repo

---
+++ src/main/java/com/zaalima/vaultcore/repo/RoleRepo.java
@@ -0,0 +1,9 @@
+package com.zaalima.vaultcore.repo;
+
+import com.zaalima.vaultcore.domain.Role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepo extends JpaRepository<Role, Long> {
+    Role findbyName(String name);
+}
---
+++ src/main/java/com/zaalima/vaultcore/repo/UserRepo.java
@@ -0,0 +1,10 @@
+package com.zaalima.vaultcore.repo;
+
+import com.zaalima.vaultcore.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findbyUsername(String username);
+
+
+}
