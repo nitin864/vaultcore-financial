1aaf8269c55fc0606756b91a0ae6751e8091f593
Nitin <rajnitin793@gmail.com>
2026-04-10 20:54:40 +0530

    replaced @ManyToAny with @ManyToMany for user-role mapping

--- src/main/java/com/zaalima/vaultcore/domain/User.java
+++ src/main/java/com/zaalima/vaultcore/domain/User.java
@@ -12,11 +12,15 @@ import java.util.Collection;
 @Entity @Data @NoArgsConstructor @AllArgsConstructor
 public class User {
     @Id
-    @GeneratedValue(strategy =  GenerationType.AUTO)
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String username;
     private String password;
-    @ManyToAny(fetch =  FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
     private Collection<Role> roles = new ArrayList<>();
 }
