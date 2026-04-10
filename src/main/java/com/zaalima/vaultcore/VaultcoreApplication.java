d42a3be9ddca8f6161f49689548805d310193c4b
Nitin <rajnitin793@gmail.com>
2026-04-10 11:54:20 +0530

    setup project db postgres, docker, spring-boot and base files

---
+++ src/main/java/com/zaalima/vaultcore/VaultcoreApplication.java
@@ -0,0 +1,13 @@
+package com.zaalima.vaultcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultcoreApplication.class, args);
+	}
+
+}
