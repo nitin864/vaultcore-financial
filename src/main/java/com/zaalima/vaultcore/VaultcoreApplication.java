0d1bb803c38c6c14603d17b2230771e76ba77a86
Nitin <rajnitin793@gmail.com>
2026-04-11 16:01:01 +0530

    added users and assign their roles using CommandLineRunner

--- src/main/java/com/zaalima/vaultcore/VaultcoreApplication.java
+++ src/main/java/com/zaalima/vaultcore/VaultcoreApplication.java
@@ -1,10 +1,13 @@
 package com.zaalima.vaultcore;
 
 import com.zaalima.vaultcore.domain.Role;
+import com.zaalima.vaultcore.domain.User;
 import com.zaalima.vaultcore.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class VaultcoreApplication {
@@ -22,7 +25,19 @@ public class VaultcoreApplication {
 			userService.saveRole(new Role(null, "ROLE_ADMIN"));
 			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
 
-		}
+			// Create users
+			userService.saveUser(new User(null, "Nitin", "nitin", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Admin", "admin", "admin", new ArrayList<>()));
+			userService.saveUser(new User(null, "Root", "root", "root", new ArrayList<>()));
+			userService.saveUser(new User(null, "Manager", "manager", "manager", new ArrayList<>()));
+
+			//Assigning Roles
+			userService.addRoleToUser("nitin", "ROLE_USER");
+			userService.addRoleToUser("admin", "ROLE_ADMIN");
+			userService.addRoleToUser("root", "ROLE_SUPER_ADMIN");
+			userService.addRoleToUser("manager", "ROLE_MANAGER");
+
+		};
 	}
 
 }
