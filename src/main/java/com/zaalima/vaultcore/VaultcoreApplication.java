c8017c05124b520bb642be71873364c7594555f4
Nitin <rajnitin793@gmail.com>
2026-04-11 16:04:04 +0530

    added missing annotations

--- src/main/java/com/zaalima/vaultcore/VaultcoreApplication.java
+++ src/main/java/com/zaalima/vaultcore/VaultcoreApplication.java
@@ -6,6 +6,7 @@ import com.zaalima.vaultcore.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import java.util.ArrayList;
 
@@ -16,6 +17,7 @@ public class VaultcoreApplication {
 		SpringApplication.run(VaultcoreApplication.class, args);
 	}
 
+	@Bean
 	CommandLineRunner run(UserService userService){
 		return args -> {
 
