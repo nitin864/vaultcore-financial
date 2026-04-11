25b01f9544bed6cede08a898b58e6f97d62528ef
Nitin <rajnitin793@gmail.com>
2026-04-12 01:25:43 +0530

    implemented authentication using tokens

--- src/main/java/com/zaalima/vaultcore/security/JwtFilter.java
+++ src/main/java/com/zaalima/vaultcore/security/JwtFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -21,7 +22,12 @@ public class JwtFilter extends UsernamePasswordAuthenticationFilter {
     }
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        return super.attemptAuthentication(request, response);
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        log.info("username is {}", username);
+        log.info("password is {}", password);
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+        return  authenticationManager.authenticate(authenticationToken);
     }
 
     @Override
