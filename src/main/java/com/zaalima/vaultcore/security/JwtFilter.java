76e64a50de59988f52f05701a0ed3987b2466c50
Nitin <rajnitin793@gmail.com>
2026-04-12 22:07:14 +0530

    forgot to set responseHeader

--- src/main/java/com/zaalima/vaultcore/security/JwtFilter.java
+++ src/main/java/com/zaalima/vaultcore/security/JwtFilter.java
@@ -55,5 +55,8 @@ public class JwtFilter extends UsernamePasswordAuthenticationFilter {
                 .withExpiresAt(new Date(System.currentTimeMillis() + 30*60*1000))
                 .withIssuer(request.getRequestURL().toString())
                 .sign(algorithm);
+        response.setHeader("access_token", access_token);
+        response.setHeader("refresh_token", refresh_token);
+
      }
 }
