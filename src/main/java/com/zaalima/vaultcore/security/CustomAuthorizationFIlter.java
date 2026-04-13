dbbc75f50ba8c6add6ddf14f82372e46cd30c909
Nitin <rajnitin793@gmail.com>
2026-04-13 14:00:12 +0530

    updates

--- src/main/java/com/zaalima/vaultcore/security/CustomAuthorizationFIlter.java
+++ src/main/java/com/zaalima/vaultcore/security/CustomAuthorizationFIlter.java
@@ -1,4 +1,5 @@
 package com.zaalima.vaultcore.security;
+import com.auth0.jwt.algorithms.Algorithm;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,9 +8,18 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
 public class CustomAuthorizationFIlter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
+       if(request.getServletPath().equals("/api/login")){
+           filterChain.doFilter(request,response);
+       }else {
+           String authorizationheaders = request.getHeader(AUTHORIZATION);
+           if (authorizationheaders != null && authorizationheaders.startsWith("Bearer "));
+           String token = authorizationheaders.substring("Bearer ".length());
+           Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+       }
     }
 }
