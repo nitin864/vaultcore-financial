75914930853502688cc919d264a06cd6c17495ec
Nitin <rajnitin793@gmail.com>
2026-04-18 20:46:41 +0530

    Implement authorities mapping and set Authentication in SecurityContextHolder

--- src/main/java/com/zaalima/vaultcore/security/CustomAuthorizationFIlter.java
+++ src/main/java/com/zaalima/vaultcore/security/CustomAuthorizationFIlter.java
@@ -7,10 +7,16 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 
+import static java.util.Arrays.stream;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 public class CustomAuthorizationFIlter extends OncePerRequestFilter {
@@ -28,6 +34,14 @@ public class CustomAuthorizationFIlter extends OncePerRequestFilter {
                DecodedJWT decodedJWT = verifier.verify(token);
                String username = decodedJWT.getSubject();
                String[] roles = decodedJWT.getClaim("roles").asArray(String.class );
+               Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+               stream(roles).forEach(role -> {
+                   authorities.add(new SimpleGrantedAuthority(role));
+               });
+               UsernamePasswordAuthenticationToken authenticationToken =
+                       new UsernamePasswordAuthenticationToken(username, null , authorities);
+               SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+               filterChain.doFilter(request, response);
            }catch (Exception exception){
 
            }
